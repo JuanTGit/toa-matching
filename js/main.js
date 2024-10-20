a92a710462785d2ce6e54c41bfed073fef53ee72
Juan <j.tejeda46@yahoo.com>
2024-10-20 18:00:04 -0500

    added comparison checks and validation

--- js/main.js
+++ js/main.js
@@ -3,29 +3,56 @@ console.log('Hello World!')
 const cards = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
 
 const gameBoard = document.querySelectorAll(".board")
-const board = document.querySelector('.board')
-console.log(board)
-console.log(gameBoard[0])
 
+let selectedCards = [];
+
+function shuffleCards(array) {
+	return array.sort(() => 0.5 - Math.random());
+}
 
 
 function createCardElements() {
-	gameBoard.forEach(gameBoard => {
-		cards.sort(() => 0.5 - Math.random());
+	gameBoard.forEach(board => {
+		const playerCards = [...cards]
+		shuffleCards(playerCards)
 
-		cards.forEach(card => {
+		playerCards.forEach(card => {
 			const cardElement = document.createElement('div');
 			cardElement.classList.add('card')
 			cardElement.dataset.value = card;
 
-			cardElement.addEventListener('click', () => {
-				cardElement.textContent = card;
-			})
+			cardElement.addEventListener('click', () => handleCardClick(cardElement, board));
 
-			gameBoard.appendChild(cardElement)
+			board.appendChild(cardElement)
 			})
 		})
 		
 }
 
-createCardElements()
+function handleCardClick(cardElement, board) {
+	if (selectedCards.length < 2) {
+		cardElement.textContent = cardElement.dataset.value;
+		selectedCards.push({ value: cardElement.dataset.value, board: board, element: cardElement });
+	}
+
+	if (selectedCards.length === 2) {
+		compareCards();
+	}
+}
+
+function compareCards() {
+	const [card1, card2] = selectedCards;
+
+	if (card1.value === card2.value) {
+		alert("It's a match!")
+	} else {
+		setTimeout(() => {
+			card1.element.textContent = '';
+			card2.element.textContent = '';
+		}, 1000)
+	}
+
+	selectedCards = []
+}
+
+createCardElements();
